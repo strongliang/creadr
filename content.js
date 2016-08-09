ffc4369c338c27eb1186120534131f5bb87650d5
Zhuang Liang <zhuang@uber.com>
2016-08-09 08:48:17 -0700

    - separate out the token from repo; - add error dialog and display function; - add error handling - clean up dead testing code

--- content.js
+++ content.js
@@ -1,3 +1,10 @@
+/*
+ * global stuff
+ *
+ * config: stores some config. this file shouldn't be checked in to git
+ * mpy: mini-pinyin.js, which is just a giant lookup table
+ * $: jquery
+ */
 // Listen for messages
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     // If the received message has the expected format...
@@ -5,15 +12,22 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
         // Call the specified callback, passing
         // the web-page's DOM content as argument
         sendResponse(document.all[0].outerHTML);
-        // sendResponse('blah from content page');
+
         parseDom();
-        // testPinyin();
     }
 });
 
-function testPinyin() {
-    var page = '<p>' + mpy('好') + '</p>';
-    $('body').html(page);
+function displayErr(msg) {
+    $('body').append(
+        '<div id="errDialog"> ' +
+        msg +
+        '<span class="close">x</span></div>'
+        );
+    $('#errDialog').slideDown('slow');
+    $('#errDialog .close').on('click', function() {
+        $('#errDialog').hide();
+    });
+
 }
 
 function parseDom() {
@@ -21,7 +35,7 @@ function parseDom() {
         url: 'https://www.readability.com/api/content/v1/parser',
         type: 'GET',
         data: {
-            token: 'faf03bf4174913773fcd36134dba8c8d595d70dc',
+            token: config.token,
             // get the url for the current doc
             url: window.location.href
         },
@@ -31,6 +45,7 @@ function parseDom() {
         },
         error: function(jqxhr, status, error) {
             console.log(status + error);
+            displayErr('readability api returned error');
         }
     });
 }
@@ -47,17 +62,28 @@ function pinynize(res) {
     content += '<div class="cread-main">';
     ptags.each(function renderParagraph(pidx, para) {
         content += '<p class="cread-paragraph">';
-        var ch = para.childNodes[0].data;
-        ch.split('').forEach(function renderTile(char, cidx) {
-            // for simplicity, take just the first option
-            var py = mpy(char)[0];
-            var pyId = 'p' + cidx + '-' + pidx;
-            var cnId = 'c' + cidx + '-' + pidx;
-            content += '<div class="cread-tile">';
-            content += '<span class="cread-py" id="' + pyId + '">' + (py ? (py + ' ') : char) + '</span>';
-            content += '<span class="cread-cn" id="' + cnId + '">' + char + '</span></div>';
-        });
-        content = content + '</p>';
+        // TODO: error handling on no content
+        var ch = para.childNodes[0].data || '';
+        if (!para) {
+            displayErr('no paragraph');
+        } else if (!para.childNodes) {
+            displayErr('no paragraph.childNodes');
+        } else if (!para.childNodes[0]) {
+            displayErr('no paragraph.childNodes[0]');
+        } else if (!para.childNodes[0].data) {
+            displayErr('no paragraph.childNodes[0].data');
+        } else {
+            ch.split('').forEach(function renderTile(char, cidx) {
+                // for simplicity, take just the first option
+                var py = mpy(char)[0];
+                var pyId = 'p' + cidx + '-' + pidx;
+                var cnId = 'c' + cidx + '-' + pidx;
+                content += '<div class="cread-tile">';
+                content += '<span class="cread-py" id="' + pyId + '">' + (py ? (py + ' ') : char) + '</span>';
+                content += '<span class="cread-cn" id="' + cnId + '">' + char + '</span></div>';
+            });
+            content = content + '</p>';
+        }
     });
     content += '</div>';
     $('body').replaceWith(content);
